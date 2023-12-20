15b0cd4a74e4c7f1fc6a47cb723cd8acb62e22c2
eldzej2137 <olosmo10@gmail.com>
2023-12-20 18:06:34 +0100

    oczywiście były tam błędy - poprawiłem je (mam nadzieję)

--- src/gauss.c
+++ src/gauss.c
@@ -1,4 +1,5 @@
 #include "gauss.h"
+#include <math.h>
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
@@ -9,7 +10,45 @@ int eliminate(Matrix *mat, Matrix *b){
     /**
   	 * Tutaj należy umieścić właściwą implemntację.
 		 */
+	int ir,ic,i;
+	// tworzymy macierz rozszerzoną
+	Matrix *ext = createMatrix(mat->r, mat->c+1);
+	for (ir=0; ir<mat->r; ir++){
+		for (ic=0; ic<mat->c; ic++)
+			ext->data[ir][ic]=mat->data[ir][ic];
+		ext->data[ir][mat->c]=b->data[ir][0];
+	}
+	int max_abs_index;
+	for (i=0; i<ext->r-1; i++){
+		// wybór elementu głównego (nie robimy tego w przedostatniej
+		// kolumnie, bo w niej zostanie tylko jeden element)
+		max_abs_index=i;
+		for (ir=i+1; ir<ext->r; ir++){
+			if (fabs(ext->data[ir][i])>fabs(ext->data[max_abs_index][i]))
+				max_abs_index=ir;
+		}
+		// zamieniamy miejscami pierwszy wiersz z tym, który zawiera element główny
+		double *tmp = malloc(ext->c*sizeof(double));
+		if (max_abs_index!=i){
+			for (ic=i; ic<ext->c; ic++){
+				tmp[ic]=ext->data[i][ic];
+				ext->data[i][ic]=ext->data[max_abs_index][ic];
+				ext->data[max_abs_index][ic]=tmp[ic];
+			}
+		}
+		free(tmp);
+		// teraz od każdego wiersza odejmujemy górny przemnożony przez
+		// odpowiednią wartość
+		for (ir=i+1; ir<ext->r; ir++){
+			for (ic=i; ic<ext->c; ic++){
+				if (ext->data[i][i]!=0)
+					ext->data[ir][ic]-=ext->data[i][ic]*ext->data[ir][i]/ext->data[i][i];
+				else
+					return 1;
+			}
+		}
+	}
 	
-		return 0;
+	return 0;
 }
 
